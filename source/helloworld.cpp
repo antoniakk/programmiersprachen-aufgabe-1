0e63112a7460a0f4c6aa3edb0d54a8d8a5ed79e0
Antonia K <antonia.krumieva@gmail.com>
2020-05-10 12:28:20 +0300

    Solve Aufgabe 1.3

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+
+int zahl1_20() {
+  bool not_found = true;
+  int number = 20;
+  while (not_found) {
+    for (int i = 1; i <= 20; ++i) {
+      if (number%i != 0) {
+        number+=20;
+        break;
+      }
+      if (i==20) {
+        not_found = false;
+      }
+    }
+  }
+  return number;
+}
+
+
 int main()
 {
-  std::cout << "Hello, World!\n";
+  std::cout << zahl1_20() << std::endl;
   return 0;
 }
