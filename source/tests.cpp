cc561ae72ba60c84976bf7f88e205468af9c42ff
Antonia K <antonia.krumieva@gmail.com>
2020-05-11 13:07:19 +0300

    Implement sum_multiples with TDD

--- source/tests.cpp
+++ source/tests.cpp
@@ -40,6 +40,19 @@ TEST_CASE("describe_checksum", "[checksum]") {
   REQUIRE(checksum(735564) == 30);
 }
 
+int sum_multiples() {
+  int sum = 0;
+  for(int i = 1; i <= 1000; ++i) {
+    if (i%3 == 0 || i%5 == 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+TEST_CASE("describe_sum_multiples", "[sum_multiples]") {
+  REQUIRE(sum_multiples() == 234168);
+}
 
 
 int main(int argc, char* argv[])
